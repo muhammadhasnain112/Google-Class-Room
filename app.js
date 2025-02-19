81a3e0aa56730cb899213c3a2653150a815b1d86
muhammadhasnain112 <muhammadhasnain12446@gmail.com>
2025-02-19 21:48:58 +0500

    Class Room

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-let btn = document.querySelector('.btn')
+let btn = document.getElementById('btn')
+let btn1 = document.getElementById('btn1')
+
+
+let token = localStorage.getItem("authToken");
+        if (token) {
+            window.location.href = "main.html";
+        }
+        
 btn.addEventListener('click',()=>{
-    window.location.href = `login.html`
-    
+    window.location.href = `SignUp.html`
 })
+// let btn1 = document.getElementById('btn')
+btn1.addEventListener('click',()=>{
+    window.location.href = `login.html`
+})
+
--- new.js
+++ new.js
@@ -1,3 +1,22 @@
+let token = localStorage.getItem("authToken");
+if (!token) {
+    window.location.href = "index.html";
+}
+
+let aside = document.getElementById('aside')
+let arr = JSON.parse(localStorage.getItem('class')) || [];
+
+for (let i = 0; i < arr.length; i++) {
+    aside.innerHTML += ` <aside>
+        <div class="div1" id="aaa">
+            <h4 id="h4">${arr[i].classname}</h4>
+            <p id="para">${arr[i].section}</p>
+        </div>
+        <h5 id="h5">Class Code : ${arr[i].Classcode} </h5>
+    </aside>`
+}
+
+
 let create = document.getElementById('create')
 let join = document.getElementById('join')
 let section = document.getElementById('sec')
@@ -12,18 +31,17 @@ create.addEventListener('click', function () {
             <input type="text" placeholder="Class Name" required id="input1"> <br>
             <input type="text" placeholder="Section" id="input2"><br>
             <input type="text" placeholder="Subject" id="input3"><br>
-            <input type="file" id="file"><br>
             <p><a href="" id="cancel">Cancel</a>&nbsp; &nbsp; &nbsp;<a href="" id="cr">Create</a></p></div>`
-    
+
     let input1 = document.getElementById('input1')
     let input2 = document.getElementById('input2')
     let input3 = document.getElementById('input3')
-   
-    
+
+
     // let input4 = document.getElementById('input4')
     let crt = document.getElementById('cr')
-    let arr = JSON.parse(localStorage.getItem('class')) || [];
     crt.addEventListener('click', function () {
+        aside.innerHTML = ``
         event.preventDefault()
         let password = ""
         let charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$&"
@@ -31,26 +49,21 @@ create.addEventListener('click', function () {
             let index = Math.floor(Math.random() * charset.length);
             password += charset[index]
         }
-        let img = document.getElementById('file')
-        let fill = img.files[0];
-        let url = URL.createObjectURL(fill);
         let obj = {
             classname: input1.value,
             section: input2.value,
             subject: input3.value,
-            img:url,
             Classcode: password,
         }
         // console.log(fill);
         arr.push(obj)
-        console.log(arr);
+        // console.log(arr);
         localStorage.setItem('class', JSON.stringify(arr))
         section.innerHTML = ``
-        let aside = document.getElementById('aside')
         let ClearAll = document.getElementById('ClearAll')
         ClearAll.style.display = 'block'
         ClearAll.addEventListener('click', () => {
-            localStorage.clear()
+            localStorage.removeItem('class')
             arr = []
             aside.innerHTML = ``
             ClearAll.style.display = 'none'
@@ -61,25 +74,55 @@ create.addEventListener('click', function () {
                 <div class="div1" id="aaa">
                     <h4 id="h4">${arr[i].classname}</h4>
                     <p id="para">${arr[i].section}</p>
+                    <p id="para">${arr[i].userName}</p>
                 </div>
                 <h5 id="h5">Class Code : ${arr[i].Classcode} </h5>
             </aside>`
-            let image = document.getElementById('aaa')
-            image.style.backgroundImage = `url(${url})`
         }
-
     })
-
-
-
-
-
     document.getElementById('cancel').addEventListener('click', () => {
         section.innerHTML = ``
     })
 
-
-
-
 })
-// localStorage.clear()
+
+
+let logout = document.getElementById('logout')
+logout.addEventListener('click', () => {
+    localStorage.removeItem("authToken");
+
+    Swal.fire("Account!", "Logout Successfuly!", "success").then(() => {
+        window.location.href = "index.html";
+    });
+})
+let classJoin = document.getElementById('join')
+classJoin.addEventListener('click', () => {
+    section.innerHTML = ` <div id="section1">
+    <p>Join class</p>
+    <input type="text" placeholder="Enter Your Name" id="newname"><br>
+    <input type="text" placeholder="Enter ID" id="ID"><br>
+    <p><a href="" id="cancel">Cancel</a>&nbsp; &nbsp; &nbsp;<a href="" id="classjoin">join</a></p></div>`
+    let ID = document.getElementById('ID')
+    let newName = document.getElementById('newname')
+
+    document.getElementById('classjoin').addEventListener('click', () => {
+        event.preventDefault()
+        let upper = newName.value.slice(0, 1).toUpperCase() + newName.value.slice(1)
+
+
+        for (let i = 0; i < arr.length; i++) {
+            if (ID.value == arr[i].Classcode) {
+                Swal.fire("Welcome!", "ID Is Correct!", "success").then(() => {
+                    arr[i]['userName'] = upper,
+                    localStorage.setItem("class", JSON.stringify(arr));
+                });
+            }else{
+                Swal.fire({
+                    title: "Sorry!",
+                    text: "ID Is Incorrect!",
+                    icon: "error"
+                });
+            }
+        }
+    })
+})
